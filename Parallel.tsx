3e15275da9920045d01b9c7e0213949d3e51eaf7
Bruno Santos <brunolsantos152@gmail.com>
2022-11-04 19:57:21 -0300

    Create Parallel.tsx

---
+++ Parallel.tsx
@@ -0,0 +1,11 @@
+import { PassportButton } from '@parallelmarkets/react'
+
+export default function ParallelButton() {
+  // const { parallel, loginStatus } = useParallel()
+
+  return (
+    <div>
+      <PassportButton />
+    </div>
+  )
+}
